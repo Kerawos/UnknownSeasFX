20a5b7645d1ed6f71493476e1f2d416b05f66746
Kerawos <Maresowa@gmail.com>
2017-12-11 11:46:50 +0100

    @add udpate Market view

--- src/main/java/pl/mareksowa/controllers/MarketController.java
+++ src/main/java/pl/mareksowa/controllers/MarketController.java
@@ -3,6 +3,9 @@ package pl.mareksowa.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 
@@ -11,13 +14,53 @@ import java.util.ResourceBundle;
 
 public class MarketController extends PlayerShipController implements Initializable {
 
+    @FXML private ImageView ivBackGround;
+    @FXML private Label lblTitle;
+    @FXML private Label lblUpperText;
+    @FXML private Label lblPriceFood;
+    @FXML private Label lblPriceWheat;
+    @FXML private Label lblPriceClothes;
+    @FXML private Label lblPriceWine;
+    @FXML private Label lblPriceMahogany;
+    @FXML private Label lblPriceDecorations;
+    @FXML private Label lblPriceSilk;
+    @FXML private Label lblPriceSpices;
+    @FXML private Button btnAddFood;
+    @FXML private Button btnAddWheat;
+    @FXML private Button btnAddClothes;
+    @FXML private Button btnAddWine;
+    @FXML private Button btnAddMahogany;
+    @FXML private Button btnAddDecorations;
+    @FXML private Button btnAddSilk;
+    @FXML private Button btnAddSpices;
     @FXML private Button btnBack;
+
+    private Image imgFood;
+    private Image imgWheat;
+    private Image imgClothes;
+    private Image imgWine;
+    private Image imgMahogany;
+    private Image imgDecorations;
+    private Image imgSilk;
+    private Image imgSpices;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         updateScene();
         buttonsRegister();
         showTutorial();
+    }
+
+    private void imgLoader(){
+        setBackgroundImage(new Image("img/BackGround.png"));
+        imgFood = new Image("img/goods/Food.png");
+        imgWheat = new Image("img/goods/Wheat.png");
+        imgClothes = new Image("img/goods/Clothes.png");
+        imgWine = new Image("img/goods/Wine.png");
+        imgMahogany = new Image("img/goods/Mahogany.png");
+        imgDecorations = new Image("img/goods/Decorations.png");
+        imgSilk = new Image("img/goods/Silk.png");
+        imgSpices = new Image("img/goods/Spices.png");
     }
 
     /**
@@ -36,6 +79,20 @@ public class MarketController extends PlayerShipController implements Initializa
         updatePlayerShip(getPLAYER_SHIP());
     }
 
+    private void updateMarketView(){
+        lblTitle.setText("Market");
+        ivBackGround.setImage(getBackgroundImage());
+        btnAddFood.setGraphic(new ImageView(imgFood));
+        btnAddWheat.setGraphic(new ImageView(imgWheat));
+        btnAddClothes.setGraphic(new ImageView(imgClothes));
+        btnAddWine.setGraphic(new ImageView(imgWine));
+        btnAddMahogany.setGraphic(new ImageView(imgMahogany));
+        btnAddDecorations.setGraphic(new ImageView(imgDecorations));
+        btnAddSilk.setGraphic(new ImageView(imgSilk));
+        btnAddSpices.setGraphic(new ImageView(imgSpices));
+        updateShipBackgroundView();
+    }
+
     private void buttonsRegister(){
 
         btnRegBack();
@@ -47,5 +104,5 @@ public class MarketController extends PlayerShipController implements Initializa
             getScene().sceneChange(dockyard, getScene().sceneNameFinderByEnum(getScene().getBackStage()));
         });
     }
-    
+
 }
