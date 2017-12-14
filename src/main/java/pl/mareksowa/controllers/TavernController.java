ece439325b9fe7c3e0ec48a2bf212679bf4ebacb
Kerawos <Maresowa@gmail.com>
2017-12-14 16:18:46 +0100

    @add TavernController

---
+++ src/main/java/pl/mareksowa/controllers/TavernController.java
@@ -0,0 +1,23 @@
+package pl.mareksowa.controllers;
+
+/**
+ * Imports section
+ */
+
+import javafx.fxml.Initializable;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Controller responsible for all crew movement. Hire, fire crew members, buy food for the crew.
+ */
+@Component
+public class TavernController extends PlayerShipController implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
